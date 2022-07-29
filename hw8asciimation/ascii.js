fb3b961bc7d4df069cee295680fee25204b08ae3
sonytaNget <sonytanget@gmail.com>
2022-07-28 20:53:41 -0500

    change animation

--- hw8asciimation/ascii.js
+++ hw8asciimation/ascii.js
@@ -1,68 +1,7 @@
-// 'esversion: 6';
-
-// window.onload = () => {
-
-//     var textArea, startButton, stopButton, animationSelector, sizeSelector, setTimeOutID, turboChecker;
-//     var animationSpeed = 250;
-    
-//     textArea = document.getElementById("textArea");
-//     startButton = document.getElementById("startButton");
-//     stopButton = document.getElementById("stopButton");
-//     animationSelector = document.getElementById("animation");
-//     sizeSelector = document.getElementById("size");
-//     turboChecker = document.getElementById("turbo");
-
-//     stopButton.disabled = true;
-//     console.log("it is here");
-
-//     // -------- Set Value from Animation -------
-//     animationSelector.onchange = () => {
-//         console.log(`Selecting ${animationSelector.value}`);
-//         textArea.value = ANIMATIONS[animationSelector.value];
-//     };
-
-//     sizeSelector.onchange = () => {
-//         console.log(`Selecting ${sizeSelector.value}`);
-//         textArea.style.fontSize = sizeSelector.value;
-//     };
-
-//     turboChecker.onchange = () => {
-//         animationSpeed = turboChecker.checked ? 50 : 250;
-//         console.log(`Turbo Checking ${turboChecker.checked} speed: ${animationSpeed}`);
-//     };
-
-//     startButton.onclick = () => {
-//         console.log("starting");
-//         stopButton.disabled = false;
-//         startButton.disabled = true;
-//         animationSelector.disabled = true;
-
-//         let texts = ANIMATIONS[animationSelector.value].split("=====\n");
-//         let idx = 0;
-        
-//         var displayFunction = function() {
-//             textArea.value = texts[idx];
-//             idx = (idx + 1) % texts.length;
-//             setTimeOutID = setTimeout(displayFunction, animationSpeed);
-//         };
-//         setTimeOutID = setTimeout(displayFunction, animationSpeed);
-//     };
-
-//     stopButton.onclick = () => {
-//         console.log("stopping");
-//         stopButton.disabled = true;
-//         startButton.disabled = false;
-//         animationSelector.disabled = false;
-//         clearTimeout(setTimeOutID);
-//         textArea.value = ANIMATIONS[animationSelector.value];
-//     };
-// };
-
-
 $(document).ready(function(){
 
     var timeOutID, selectedAnimation, selectedSize;
-    var speedAnimation = 10;
+    var speedAnimation = 50;
     
     var textArea = document.getElementById("textArea");
 
@@ -91,6 +30,11 @@ $(document).ready(function(){
         disableButton($("#startButton"));
         enableButton($("#stopButton"));
         disableButton($("#animation"));
+
+        if (!selectedAnimation) {
+            selectedAnimation = "BLANK"
+        }
+       console.log(selectedAnimation);
         
         timeOutID = setInterval(displayFunction, speedAnimation);
    });
@@ -117,13 +61,13 @@ $(document).ready(function(){
 
     $("#turbo").click(function(){
         if ($("#turbo").is(':checked')) {
-            speedAnimation = 10;
+            speedAnimation = 50;
             console.log("checked");
             console.log(speedAnimation);
             clearInterval(timeOutID);
             timeOutID = setInterval(displayFunction, speedAnimation);
         } else {
-            speedAnimation = 500;
+            speedAnimation = 250;
             console.log("uncheck");
             console.log(speedAnimation);
             clearInterval(timeOutID);
