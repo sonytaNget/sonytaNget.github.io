54b8af45a84e32c9e4edbf9cf9e974d155d1c705
sonytaNget <sonytanget@gmail.com>
2022-07-28 23:57:10 -0500

    add mouse lab

--- mousemazelab/maze.js
+++ mousemazelab/maze.js
@@ -1,10 +1,38 @@
 $(document).ready(function(){
 
+    var hitAnyWall = false;
+    var start = false;
 
-    $('.boundary').hover(function(){
-        $(this).css("background-color", "red");
-      },
-      function(){
-        $(this).css("background-color", "blue");
-      });
+    $('.boundary').mouseover(function(){
+      if (start === true) {
+        $(".boundary").css("background-color", "");
+        $(".boundary").addClass("youlose");
+        $("#status").text("Sorry, you lost :(");
+        hitAnyWall = true;
+      }
+    });
+
+    $("#maze").mouseleave(function(){
+      
+      if (start === true) {
+        $("#status").text("Sorry, you lost :(");
+      }
+    });
+
+    $("#start").click(function(){
+
+      start = true;
+      $("#status").text("Click the " + "\"S\"" + " to begin.");
+      $(".boundary").css("background-color", "#eeeeee");
+    }); 
+
+    $("#end").mouseover(function(){
+
+      console.log(start);
+      if (start === true && hitAnyWall === false) {
+        $("#status").text("You win :D");
+        hitAnyWall = false;
+        start = false;
+      }
+    }); 
 });
